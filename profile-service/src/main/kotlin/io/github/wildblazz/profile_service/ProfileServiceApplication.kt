be5156c7611d448006a98a04e0e6bcf8e595e0b4
okovalenko <wildblazz@gmail.com>
2025-03-15 15:26:45 +0100

    Profile service init

---
+++ profile-service/src/main/kotlin/io/github/wildblazz/profile_service/ProfileServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.github.wildblazz.profile_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProfileServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProfileServiceApplication>(*args)
+}
---
+++ profile-service/src/test/kotlin/io/github/wildblazz/profile_service/ProfileServiceApplicationTests.kt
@@ -0,0 +1,14 @@
+package io.github.wildblazz.profile_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+	}
+
+}
